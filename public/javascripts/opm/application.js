2e4e3ee6a71649e210573de87dee2cf2b3061022
Shaoshing <sxlee87@gmail.com>
2013-01-09 16:15:43 +0800

    Generate and assign share urls to share links respectively; according to the current artilce title and url using Javascript.

---
+++ public/javascripts/opm/application.js
@@ -0,0 +1,20 @@
+jQuery(function(){
+  jQuery('.share').hover(function(){jQuery(this).toggleClass('active');});
+  jQuery('.share').each(function(){
+    var shareButton = jQuery(this);
+    var articleEle = shareButton.parent('.article');
+    var articleUrl = encodeURIComponent(articleEle.find('.page-header a').attr('href'));
+    var articleTitle = encodeURIComponent(articleEle.find('.page-header a').text());
+
+    jQuery.each({
+      'service-fb': 'http://www.facebook.com/sharer.php?t=#TITLE#&u=#URL#',
+      'service-t':  'https://twitter.com/home?status=#TITLE#:#URL#',
+      'service-li': 'http://www.linkedin.com/shareArticle?mini=true&url=#URL#&title=#TITLE#',
+      'service-r':  'http://www.reddit.com/submit?url=#URL#&title=#TITLE#',
+      'service-g':  'https://plus.google.com/share?url=#URL#&t=#TITLE#'
+    }, function(serviceClass, urlPattern){
+      var shareUrl = urlPattern.replace('#URL#', articleUrl).replace('#TITLE#', articleTitle);
+      shareButton.find('.'+serviceClass+' a').attr('href', shareUrl);
+    });
+  });
+});
